482c0a537b85f6c25b98871622c3d242f255686a
tn <2825557671@qq.com>
2025-03-12 18:17:23 +0800

    init2

--- ToDoList/vite.config.js
+++ ToDoList/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/todo-frontend/',  // 确保这里的名称与你的仓库名完全一致
+  base: '/ToDoList/',  // 确保这里的名称与你的仓库名完全一致
 }) 
